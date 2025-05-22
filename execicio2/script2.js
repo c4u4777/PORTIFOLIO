1fbf20017cc51b0f2d2f7b330376bbdcb491de1b
c4u4777 <amaralcaua66@gmail.com>
2025-05-22 09:56:01 -0300

    Add files via upload

---
+++ execicio2/script2.js
@@ -0,0 +1,47 @@
+function somar(){
+alert("digite o numero que vc quer somar aqui ")
+
+
+  let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+  let num2 = Number(window.prompt("Digite o segundo número:"));
+
+  let resultado = num1 + num2;
+
+  alert("o resultado é " + resultado + ".")
+
+}
+function subtrair() {
+    alert("digite o numero que vc quer subtrair aqui ")
+
+  let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+  let num2 = Number(window.prompt("Digite o segundo número:"));
+
+  let resultado = num1 - num2
+
+  alert ("o resultado é " + resultado )
+  
+
+}
+
+function dividir() {
+  alert("digite o numero que vc quer dividir aqui ")
+
+let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+let num2 = Number(window.prompt("Digite o segundo número:"));
+
+let resultado = num1 / num2
+
+alert ("o resultado é " + resultado )
+
+
+}
+function multiplicar() {
+  alert("digite o numero que vc quer multiplicar aqui ")
+
+let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+let num2 = Number(window.prompt("Digite o segundo número:"));
+
+let resultado = num1 * num2
+
+alert ("o resultado é " + resultado )
+}
--- exercício1/script.js
+++ exercício1/script.js
@@ -13,13 +13,13 @@ parOuImpar = resultado % 2;
 if(parOuImpar == 0) {
 alert(" o número " + resultado + " é par");
 }else{
-alert(" o número " + resultado + " é impar");
+alert(" o número " + resultado + " é impar");   
 
 
 }
 
 if(resultado > 10) {
-alert("O número " + resultado + "é maior do que 10")
+alert("O número " + resultado + " é maior do que 10")
 } else {
-alert("O número " + resultado + "é menor que 10")
+alert("O número " + resultado + " é menor que 10")
 }
