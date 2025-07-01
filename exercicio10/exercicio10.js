035faae7be9832bf9943091d1d948bce04dd3be5
caua <amaralcaua66@gmail.com>
2025-07-01 10:52:24 -0300

    Add files via upload

---
+++ exercicio10/exercicio10.js
@@ -0,0 +1,20 @@
+function contar() {
+
+let mostraHTML = document.getElementById("mostraHTML")
+
+let contador = 1;
+
+while (contador <= 10){
+
+mostraHTML.innerHTML += `${contador}`;
+contador++;
+} 
+
+function parOuImpar() {
+let
+
+
+
+}
+
+}
---
+++ exercicio5/script.js
@@ -0,0 +1,15 @@
+function valor (){
+
+let num1=Number(window.prompt("digite o primeiro numero"));
+let num2=Number(window.prompt("digite o segundo numero"));
+
+   if (num1 > num2){
+
+     alert("o numero" + num1 + " é o maior ")
+
+   } else{
+
+     alert ("o numero" + num2 + "é o maior" )
+    
+   }
+}
